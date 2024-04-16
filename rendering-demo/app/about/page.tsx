1d5de8c9b5a0cc20d6958cb022bbad458e66b099
Mario Lazzari <mario.lazzari@gmail.com>
2024-04-16 10:04:41 +0200

    Done lesson 53

--- rendering-demo/app/about/page.tsx
+++ rendering-demo/app/about/page.tsx
@@ -1,7 +1,16 @@
-function AboutPage() {
-  console.log("About server");
+import { cookies } from "next/headers";
 
-  return <div>About Page</div>;
+function AboutPage() {
+  const cookieStore = cookies();
+  const theme = cookieStore.get("theme");
+  console.log("About server theme:", theme);
+
+  return (
+    <div className="flex flex-col justify-center items-center w-screen h-screen">
+      <h1 className="text-xl">About Page</h1>
+      <h2 className="text-lg">{new Date().toLocaleTimeString()}</h2>
+    </div>
+  );
 }
 
 export default AboutPage;
